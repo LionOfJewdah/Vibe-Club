3bb571e603a2a595d22d18dedd81fc865bfc0a3f
David Paul Silverstone <dsilvers@usc.edu>
2018-03-27 17:51:21 -0700

    periodic 5 second message sends from preprogrammed data

--- send_mqtt.py
+++ send_mqtt.py
@@ -1,16 +1,34 @@
 import paho.mqtt.publish as publish
 import time
 import sys
+from sched import scheduler
+from datetime import datetime
+from array import array
+
+JSON_template = """{{
+	\"numberOfPeople\": {0},
+	\"venue_ID\": 1,
+	\"camera_ID\": 1
+}}"""
 
 def SendMessage(host, payload, topic="demo/davids_pi"):
-    print("Sending message {0} to host {1} under topic {2}".format(
-        payload, host, topic)
-    )
-    publish.single(topic, payload, hostname=host)
+	print("Sending message \"{0}\" to host {1} under topic {2}".format(
+		payload, host, topic)
+	)
+	publish.single(topic, payload, hostname=host)
+
+def SendPopulationCount(host, json_src, count, topic="demo/davids_pi"):
+	SendMessage(host, json_src.format(count), topic)
 
 host = sys.argv[1]
-IFuckingFeelLikeIt = True
-while IFuckingFeelLikeIt:
-    SendMessage(host, '{"numberOfPeople": 5}')
-    time.sleep(4)
+populations = array('H', [4, 4, 5, 6, 6, 5, 6, 5, 5, 4, 3, 4])
+populationCount = 4
+timer = scheduler(time.time, time.sleep)
+def PeriodicSend(task, it):
+	populationCount = populations[it]
+	SendPopulationCount(host, JSON_template, populationCount)
+	it = (it + 1) % (60 / 5)
+	timer.enter(5, 1, PeriodicSend, (task, it,))
 
+timer.enter(0, 1, PeriodicSend, (timer, 0))
+timer.run()
