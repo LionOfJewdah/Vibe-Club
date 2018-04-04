440317adf4828d0b600d78bedff16faf4faa8175
David Silverstone <dsilvers@usc.edu>
2018-04-03 17:49:08 -0700

    MQTT updates on back end

--- camera_send.py
+++ camera_send.py
@@ -6,18 +6,21 @@ from datetime import datetime
 from array import array
 
 JSON_template = """{{
-	\"numberOfPeople\": {0},
-	\"venue_ID\": 1,
-	\"camera_ID\": 1
+	\"numberOfPeople\": {0}
 }}"""
 
-def SendMessage(host, payload, topic="vibe/pi_01"):
-	print("Sending message \"{0}\" to host {1} under topic {2}".format(
+venue_ID = 1
+camera_ID = 1
+sensorType = "camera"
+myTopic = "vibe/venue_{0}/{1}_{2}".format(venue_ID, sensorType, camera_ID)
+
+def SendMessage(host, payload, topic = myTopic):
+	print("Sending message \"{0}\" to host {1} under topic '{2}'".format(
 		payload, host, topic)
 	)
-	publish.single(topic, payload, hostname=host)
+	publish.single(topic, payload, hostname = host)
 
-def SendPopulationCount(host, json_src, count, topic="vibe/pi_01"):
+def SendPopulationCount(host, json_src, count, topic = myTopic):
 	SendMessage(host, json_src.format(count), topic)
 
 if (len (sys.argv) > 1):
@@ -31,7 +34,7 @@ timer = scheduler(time.time, time.sleep)
 def PeriodicSend(task, it):
 	populationCount = populations[it]
 	SendPopulationCount(host, JSON_template, populationCount)
-	it = (it + 1) % (60 / 5)
+	it = int((it + 1) % (60 / 5))
 	timer.enter(5, 1, PeriodicSend, (task, it,))
 
 timer.enter(0, 1, PeriodicSend, (timer, 0))
